68a42c04d4388e8efeb1b57bbd6f2002c675d50f
Nicholas Kotsos <nick.kotsos@gmail.com>
2020-09-24 20:23:03 +0000

    got correct API URL,

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,6 +1,16 @@
 const request = require('request');
 // cut full cat name down to first four letters
 let fullCatName = process.argv[2];
-let catName = fullCatName.split('').slice(0,4).join('')
+let catName = fullCatName.split('').slice(0, 4).join('')
 console.log(catName)
-request(`https://api.thecatapi.com/v1/breeds/search?breed_ids=${catName}`)
+
+request(`https://api.thecatapi.com/v1/images/search?breed_ids=Sibe`, (err, resp, body) => {
+if (err){
+  console.log(err)
+} else {
+  console.log(body)
+
+}
+}
+
+)
