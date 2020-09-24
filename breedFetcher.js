ba30de36380835f947f34089e4379bf3f6e9c9a0
Nicholas Kotsos <nick.kotsos@gmail.com>
2020-09-24 20:41:22 +0000

    removed old console. logs

--- breedFetcher.js
+++ breedFetcher.js
@@ -2,18 +2,18 @@ const request = require('request');
 // cut full cat name down to first four letters
 let fullCatName = process.argv[2];
 let catName = fullCatName.split('').slice(0, 4).join('')
-// console.log(catName)
+
 
 request(`https://api.thecatapi.com/v1/images/search?breed_ids=${catName}`, (err, resp, body) => {
   if (err) {
-    console.log(err)
-    return
+    console.log(err);
+    return;
   }
   const data = JSON.parse(body);
   if (data[0]) {
     console.log(data[0].breeds[0].description);
   } else {
-    console.log(`Oh meow! That breed wasn't found. Please try again`)
+    console.log(`Oh meow! That breed wasn't found. Please try again`);
   }
 }
 
