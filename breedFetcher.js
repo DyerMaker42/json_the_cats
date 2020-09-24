fd58fc14125c3ad5de342dfe75dde2fd135747ba
Nicholas Kotsos <nick.kotsos@gmail.com>
2020-09-24 20:17:21 +0000

    cleaned up input to just the first four letters of the request (capitals are ignored)

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,5 +1,6 @@
 const request = require('request');
 // cut full cat name down to first four letters
-let breed = process.argv(2)
-
-request(`https://api.thecatapi.com/v1/breeds/search`)
+let fullCatName = process.argv[2];
+let catName = fullCatName.split('').slice(0,4).join('')
+console.log(catName)
+//request(`https://api.thecatapi.com/v1/breeds/search`)
